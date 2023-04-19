38c882ac6aba8a59653da7fbc8756fad92f5ea52
Dharan7635 <dharanitharan1477@gmail.com>
2023-04-19 15:22:48 +0000

    commmented console logs

--- SPS.js
+++ SPS.js
@@ -7,20 +7,19 @@ if(choice)
 if(userchoice === "STONE" || 
    userchoice === "PAPER" || 
    userchoice === "SCISSORS"){
-    console.log("userChoice=",userchoice);
+    // console.log("userChoice=",userchoice);
     let computer =["STONE","PAPER","SCISSORS"];
     //GENERATING INDEX NUMBER TO PICK ARRAY FOR COMPUTER CHOICE
     let indexno = Math.floor((Math.random())*3);
-    console.log(indexno);
+    // console.log(indexno);
     let computerChoice = (computer[indexno]);
-console.log("computerChoice=",computer[indexno]);
+// console.log("computerChoice=",computer[indexno]);
 //PLAY
     let winner = userchoice === computerChoice ?  `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nGAME TIE`
     : userchoice === "PAPER" && computerChoice === "STONE" ? 
     `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nYOU WIN`
     : userchoice === "STONE" && computerChoice === "SCISSORS" ?  `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nYOU WIN`
     : userchoice === "SCISSORS" && computerChoice === "PAPER" ?  `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nYOU WIN` :  `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nCOMPUTER WIN --BETTER LUCK NEXT TIME :)`;
-    // alert("YourChoice IS "+userchoice+" & THE Computerchoice IS "+computerChoice);
     alert(winner);
     let playagain = confirm("PLAY AGAIN");
     if(playagain){
