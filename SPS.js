a601afbbd675241f9b5c05f56ed9151479a53e0c
Dharan7635 <dharanitharan1477@gmail.com>
2023-04-19 15:17:18 +0000

    Play again introduced & userchoice & computer choice will be displayed

--- SPS.js
+++ SPS.js
@@ -2,30 +2,41 @@ let choice = confirm("Shall we play a game?");
 if(choice)
 {
     //GETTING INPUT FROM USER
-    let userchoice = prompt("Enter your choice STONE/PAPER/SISSOR");
+    let userchoice = prompt("Enter your choice STONE/PAPER/SCISSORS");
+    console.log(userchoice);
 if(userchoice === "STONE" || 
    userchoice === "PAPER" || 
-   userchoice === "SISSOR"){
+   userchoice === "SCISSORS"){
     console.log("userChoice=",userchoice);
-    let computer =["STONE","PAPER","SISSOR"];
+    let computer =["STONE","PAPER","SCISSORS"];
     //GENERATING INDEX NUMBER TO PICK ARRAY FOR COMPUTER CHOICE
     let indexno = Math.floor((Math.random())*3);
     console.log(indexno);
     let computerChoice = (computer[indexno]);
 console.log("computerChoice=",computer[indexno]);
 //PLAY
-    let winner = userchoice === computerChoice ? "GAME TIE" 
-    : userchoice === "PAPER" && computerChoice === "STONE" ? "YOU WIN"
-    : userchoice === "STONE" && computerChoice === "SISSOR" ? "YOU WIN"
-    : userchoice === "SISSOR" && computerChoice === "PAPER" ? "YOU WIN" : "COMPUTER WIN --BETTER LUCK NEXT TIME :)";
-    alert("YourChoice IS "+userchoice+" & THE Computerchoice IS "+computerChoice);
+    let winner = userchoice === computerChoice ?  `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nGAME TIE`
+    : userchoice === "PAPER" && computerChoice === "STONE" ? 
+    `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nYOU WIN`
+    : userchoice === "STONE" && computerChoice === "SCISSORS" ?  `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nYOU WIN`
+    : userchoice === "SCISSORS" && computerChoice === "PAPER" ?  `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nYOU WIN` :  `Your Choice:${userchoice}\nComputer choice:${computerChoice} \nCOMPUTER WIN --BETTER LUCK NEXT TIME :)`;
+    // alert("YourChoice IS "+userchoice+" & THE Computerchoice IS "+computerChoice);
     alert(winner);
-} 
+    let playagain = confirm("PLAY AGAIN");
+    if(playagain){
+        location.reload()
+    }
+}
+else if (userchoice === null)
+{
+      alert("May be you Changed your mind (:")
+}
 else
 {
     alert("Please enter correct choice/use uppercase only");
+    location.reload();
 }
 }
 else{
-    alert("Thank you");
+    alert("OK!.May be next time");
 }
